fbe724fc669142885366a09778defa49e58bcd79
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-12-07 14:37:39 +0300

    Initial Commit

---
+++ HiringTestAppMarkent/HiringTestAppMarkentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiringTestAppMarkentApp.swift
+//  HiringTestAppMarkent
+//
+//  Created by Sergei Poluboiarinov on 2022-12-07.
+//
+
+import SwiftUI
+
+@main
+struct HiringTestAppMarkentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
