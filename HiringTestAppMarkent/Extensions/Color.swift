e1b38ad7bad837da23ee98c8aac3a5639644cdee
Sergei Poluboiarinov <sihtmark@icloud.com>
2023-07-14 16:56:43 +0300

    Icons, colors, launch screen Icons are moved to their own folder and added new extension with Image structure with all icons Colors are moved to their own folder and added new extension with color structure with all colors Added launch screen

--- HiringTestAppMarkent/Extensions/Color.swift
+++ HiringTestAppMarkent/Extensions/Color.swift
@@ -5,4 +5,15 @@
 //  Created by Sergei Poluboiarinov on 14.07.2023.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let backgroundBlue = Color("DarkBlue")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
--- HiringTestAppMarkent/Extensions/Image.swift
+++ HiringTestAppMarkent/Extensions/Image.swift
@@ -5,4 +5,27 @@
 //  Created by Sergei Poluboiarinov on 14.07.2023.
 //
 
-import Foundation
+import SwiftUI
+
+extension Image {
+    static let icon = Icon()
+}
+
+struct Icon {
+    let arrow = Image("arrow")
+    let bin = Image("bin")
+    let books = Image("books")
+    let camera = Image("camera")
+    let cart = Image("cart")
+    let chip = Image("chip")
+    let computer = Image("computer")
+    let disk = Image("disk")
+    let ellipse = Image("ellipse")
+    let filter = Image("filter")
+    let heart = Image("heart")
+    let memory = Image("memory")
+    let metka = Image("metka")
+    let phone = Image("phone")
+    let profile = Image("profile")
+    let qrCode = Image("qrCode")
+}
