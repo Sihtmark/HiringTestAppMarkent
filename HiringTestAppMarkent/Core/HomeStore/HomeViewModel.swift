0c99f5e6b9d48674dd6ca40e684e997e6fff9a03
Sergei Poluboiarinov <sihtmark@icloud.com>
2023-07-24 15:04:46 +0300

    Networking commented functions moved to HomeViewModel and adopted

--- HiringTestAppMarkent/Core/HomeStore/HomeViewModel.swift
+++ HiringTestAppMarkent/Core/HomeStore/HomeViewModel.swift
@@ -17,6 +17,8 @@ final class HomeViewModel: ObservableObject {
     @Published var image: UIImage = UIImage()
     @Published var isFavorite: Bool = false
     @Published var favoritesBestSellers = Set<Int>()
+    @Published var singleProduct: Product? = nil
+    @Published var user: User? = nil
     
     func getAllProducts() async {
         do {
@@ -36,6 +38,93 @@ final class HomeViewModel: ObservableObject {
             let responseObject = try JSONDecoder().decode(Products.self, from: data)
             await MainActor.run {
                 self.hotSales = responseObject.products
+            }
+        } catch {
+            print(URLError(.cannotDecodeRawData))
+        }
+    }
+    
+    func getSingleProduct() async {
+        do {
+            let data = try await APIRequestDispatcher.request(apiRouter: .getSingleProduct)
+            let responseObject = try JSONDecoder().decode(Product.self, from: data)
+            await MainActor.run {
+                self.singleProduct = responseObject
+            }
+        } catch {
+            print(URLError(.cannotDecodeRawData))
+        }
+    }
+    
+    func getUser(username: String, password: String) async {
+        do {
+            let data = try await APIRequestDispatcher.request(apiRouter: .auth(username: username, password: password))
+            let responseObject = try JSONDecoder().decode(User.self, from: data)
+            await MainActor.run {
+                self.user = responseObject
+            }
+        } catch {
+            print(URLError(.cannotDecodeRawData))
+        }
+    }
+    
+    func searchProducts(product: String) async -> [Product] {
+        do {
+            let data = try await APIRequestDispatcher.request(apiRouter: .searchProducts(product: product))
+            let responseObject = try JSONDecoder().decode(Products.self, from: data)
+            return responseObject.products
+        } catch {
+            print(URLError(.cannotDecodeRawData))
+        }
+        return []
+    }
+    
+    func getProductCategories() async -> [String] {
+        do {
+            let data = try await APIRequestDispatcher.request(apiRouter: .getProductCategories)
+            let responseObject = try JSONDecoder().decode([String].self, from: data)
+            return responseObject
+        } catch {
+            print(URLError(.cannotDecodeRawData))
+        }
+        return []
+    }
+    
+    func addProduct(id: Int, title: String, description: String, price: Int, discountPercentage: Double, rating: Double, stock: Int, brand: String, category: String, thumbnail: String, images: [String]
+    ) async {
+        do {
+            let data = try await APIRequestDispatcher.request(
+                apiRouter: .addProduct(id: id, title: title, description: description, price: price, discountPercentage: discountPercentage, rating: rating, stock: stock, brand: brand, category: category, thumbnail: thumbnail, images: images))
+            let responseObject = try JSONDecoder().decode(Product.self, from: data)
+        } catch {
+            print(URLError(.cannotDecodeRawData))
+        }
+    }
+    
+    func updateProduct(title: String) async {
+        do {
+            let data = try await APIRequestDispatcher.request(apiRouter: .updateProduct(title: title))
+            let responseObject = try JSONDecoder().decode(Product.self, from: data)
+        } catch {
+            print(URLError(.cannotDecodeRawData))
+        }
+    }
+    
+    func deleteProduct() async {
+        do {
+            let data = try await APIRequestDispatcher.request(apiRouter: .deleteProduct)
+            let responseObject = try JSONDecoder().decode(Product.self, from: data)
+        } catch {
+            print(URLError(.cannotDecodeRawData))
+        }
+    }
+    
+    func getProductsWithAuthToken(token: String) async throws {
+        do {
+            let data = try await APIRequestDispatcher.request(apiRouter: .getProductsWithAuthToken(token: token))
+            let responseObject = try JSONDecoder().decode(Products.self, from: data)
+            await MainActor.run {
+                self.bestSellers = responseObject.products
             }
         } catch {
             print(URLError(.cannotDecodeRawData))
--- HiringTestAppMarkent/Managers/NetworkingManager.swift
+++ HiringTestAppMarkent/Managers/NetworkingManager.swift
@@ -118,14 +118,12 @@ final class APIRequestDispatcher {
 //        components.queryItems = apiRouter.parameters
         
         guard let url = components.url else { throw APIRequestError.badUrl }
-        print(components.url!)
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = apiRouter.method
         
         switch apiRouter {
         case .getProducts, .getSingleProduct, .searchProducts, .getProductCategories, .getProductsOfCategory, .deleteProduct:
-            print("We reached")
-            
+            break
         case .auth(let username, let password):
             urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
             let json: [String: String] = ["username": username, "password": password]
@@ -157,7 +155,6 @@ final class APIRequestDispatcher {
             urlRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
             urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
         }
-        print("asdf")
         let session = URLSession(configuration: .default)
         
         return try await withCheckedThrowingContinuation { continuation in
@@ -179,202 +176,4 @@ final class APIRequestDispatcher {
 
 enum APIRequestError: Error {
     case badUrl, noData
-}
-
-//final class ProductsClient {
-//    
-//    static let shared = ProductsClient()
-//    
-//    func getProducts() async -> [Product] {
-//        do {
-//            let data = try await APIRequestDispatcher.request(apiRouter: .getProducts)
-//            let responseObject = try JSONDecoder().decode(Products.self, from: data)
-//            return responseObject.products
-//        } catch {
-//            
-//        }
-//    }
-//}
-//
-//protocol SingleProductProtocol {
-//    func getSingleProduct() async throws -> Product
-//}
-//
-//final class SingleProductClient: SingleProductProtocol {
-//    
-//    static let shared = SingleProductClient()
-//    
-//    func getSingleProduct() async throws -> Product {
-//        let response: Product = try await APIRequestDispatcher.request(apiRouter: .getSingleProduct)
-//        return response
-//    }
-//}
-//
-//protocol UserProtocol {
-//    func getUser(username: String, password: String) async throws -> User
-//}
-//
-//final class UserClient: UserProtocol {
-//    
-//    static let shared = UserClient()
-//    
-//    func getUser(username: String, password: String) async throws -> User {
-//        let response: User = try await APIRequestDispatcher.request(apiRouter: .auth(username: username, password: password))
-//        return response
-//    }
-//}
-//
-//protocol SearchProductsProtocol {
-//    func searchProducts(product: String) async throws -> [Product]
-//}
-//
-//final class SearchProductsClient: SearchProductsProtocol {
-//    
-//    static let shared = SearchProductsClient()
-//    
-//    func searchProducts(product: String) async throws -> [Product] {
-//        let response: Products = try await APIRequestDispatcher.request(apiRouter: .searchProducts(product: product))
-//        return response.products
-//    }
-//}
-//
-//protocol GetProductCategoriesProtocol {
-//    func getProductCategories() async throws -> [String]
-//}
-//
-//final class GetProductCategoriesClient: GetProductCategoriesProtocol {
-//    
-//    static let shared = GetProductCategoriesClient()
-//    
-//    func getProductCategories() async throws -> [String] {
-//        let response: [String] = try await APIRequestDispatcher.request(apiRouter: .getProductCategories)
-//        return response
-//    }
-//}
-//
-//protocol GetProductsOfCategoryProtocol {
-//    func getProductsOfCategory(category: String) async throws -> [Product]
-//}
-//
-//final class GetProductsOfCategoryClient: GetProductsOfCategoryProtocol {
-//    
-//    static let shared = GetProductsOfCategoryClient()
-//    
-//    func getProductsOfCategory(category: String) async throws -> [Product] {
-//        let response: Products = try await APIRequestDispatcher.request(apiRouter: .getProductsOfCategory(category: category))
-//        return response.products
-//    }
-//}
-//
-//protocol AddProductProtocol {
-//    func addProduct(id: Int, title: String, description: String, price: Int, discountPercentage: Double, rating: Double, stock: Int, brand: String, category: String, thumbnail: String, images: [String]
-//    ) async throws -> Product
-//}
-//
-//final class AddProductClient: AddProductProtocol {
-//    
-//    static let shared = AddProductClient()
-//    
-//    func addProduct(id: Int, title: String, description: String, price: Int, discountPercentage: Double, rating: Double, stock: Int, brand: String, category: String, thumbnail: String, images: [String]
-//    ) async throws -> Product {
-//        let response: Product = try await APIRequestDispatcher.request(
-//            apiRouter: .addProduct(id: id, title: title, description: description, price: price, discountPercentage: discountPercentage, rating: rating, stock: stock, brand: brand, category: category, thumbnail: thumbnail, images: images))
-//        return response
-//    }
-//}
-//
-//protocol UpdateProductProtocol {
-//    func updateProduct(title: String) async throws -> Product
-//}
-//
-//final class UpdateProductClient: UpdateProductProtocol {
-//    
-//    static let shared = UpdateProductClient()
-//    
-//    func updateProduct(title: String) async throws -> Product {
-//        let response: Product = try await APIRequestDispatcher.request(apiRouter: .updateProduct(title: title))
-//        return response
-//    }
-//}
-//
-//protocol DeleteProductProtocol {
-//    func deleteProduct() async throws -> Product
-//}
-//
-//final class DeleteProductClient: DeleteProductProtocol {
-//    
-//    static let shared = DeleteProductClient()
-//    
-//    func deleteProduct() async throws -> Product {
-//        let response: Product = try await APIRequestDispatcher.request(apiRouter: .deleteProduct)
-//        return response
-//    }
-//}
-//
-//protocol GetProductsWithAuthTokenProtocol {
-//    func getProductsWithAuthToken(token: String) async throws -> [Product]
-//}
-//
-//final class GetProductsWithAuthTokenClient: GetProductsWithAuthTokenProtocol {
-//    
-//    static let shared = GetProductsWithAuthTokenClient()
-//    
-//    func getProductsWithAuthToken(token: String) async throws -> [Product] {
-//        let response: Products = try await APIRequestDispatcher.request(apiRouter: .getProductsWithAuthToken(token: token))
-//        return response.products
-//    }
-//}
-
-extension APIRouter {
-//    var parameters: [URLQueryItem] {
-//        switch self {
-//        case .getProducts:
-//            return []
-//        case .auth:
-//            return []
-//        case .getSingleProduct:
-//            return []
-//        case .searchProducts(let product):
-//            return []
-//        case .getProductCategories:
-//            return []
-//        case .getProductsOfCategory(let category):
-//            return []
-//        case .addProduct:
-//            return []
-//        case .updateProduct:
-//            return []
-//        case .deleteProduct:
-//            return []
-//        case .getProductsWithAuthToken:
-//            return []
-//        }
-//    }
-}
-
-extension APIRequestDispatcher {
-    //func signIn() {
-    //    let json: [String: String] = ["username": "kminchelle", "password": "0lelplR"]
-    //    let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed)
-    //    let url = URL(string: "https://dummyjson.com/auth/login")!
-    //    var request = URLRequest(url: url)
-    //    request.httpMethod = "POST"
-    //    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    //    request.httpBody = jsonData
-    //    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-    //        print("HTTPResponse: \(String(describing: response))")
-    //        guard let data = data, error == nil else {
-    //            print(error ?? "No data")
-    //            return
-    //        }
-    //        let responseJSON = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
-    //        if let responseJSON = responseJSON as? [String: Any] {
-    //            print(responseJSON)
-    //            // here I can
-    //        } else {
-    //
-    //        }
-    //    }
-    //    task.resume()
-    //}
 }
